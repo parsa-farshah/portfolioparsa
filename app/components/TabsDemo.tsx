9fabb97b26a096f8ba76622c5a7d85312db70a79
parsa <parsafarashah@gmail.com>
2026-09-01 22:22:28 +0330

    ADD(add card for tabsDemo)

--- app/components/TabsDemo.tsx
+++ app/components/TabsDemo.tsx
@@ -1,103 +1,100 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import myData from "../store";
+import Card from "./shared/Card";
 
 export function TabsDemo() {
   const { theme } = myData();
   return (
     <Tabs
-      defaultValue="overview"
+      defaultValue="Monthly"
       className="w-full mx-auto flex justify-center "
     >
       <TabsList className="!w-fit !h-fit py-2 border border-[#a1a1a14f] bg-[#fff0] mx-auto !px-4 flex gap-2 ">
         <TabsTrigger
-          value="overview"
+          value="Monthly"
           className={`w-fit h-fit px-4 py-2 data-[state=active]:bg-[#0055fe] data-[state=active]:!text-white  cursor-pointer ${theme ? " !text-black" : "!text-white"}`}
         >
-          Normal
+          Monthly
         </TabsTrigger>
 
         <TabsTrigger
-          value="analytics"
+          value="Yearly"
           className={`w-fit h-fit px-4 py-2 data-[state=active]:bg-[#0055fe] data-[state=active]:!text-white  cursor-pointer ${theme ? " !text-black" : "!text-white"}`}
         >
           Professional
         </TabsTrigger>
       </TabsList>
-      {/* Overview cards */}
+      {/* Normal cards */}
       <TabsContent
-        className="mt-20 w-fit mx-auto flex justify-center flex-wrap gap-10"
-        value="overview"
+        className="mt-20 w-fit mx-auto flex justify-center flex-wrap gap-5"
+        value="Monthly"
       >
-        {/* left side */}
-        <Card
-          className={`border border-[#a1a1a14f] bg-[#fff0]  w-[90%] md:w-[40%] backdrop-blur-lg ${theme ? "bg-[#d3d3d33a] text-black" : "bg-[#020202a8] text-white"}`}
-        >
-          <CardHeader>
-            <CardTitle>Freelane</CardTitle>
-            <CardDescription className="ml-1">
-              if you have a normal project
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="text-muted-foreground text-sm ml-1">
-            You have 12 active projects and 3 pending tasks.
-          </CardContent>
-        </Card>
-        {/* right side */}
-        <Card
-          className={`border border-[#a1a1a14f] bg-[#fff0]  w-[90%] md:w-[40%] backdrop-blur-lg ${theme ? "bg-[#d3d3d33a] text-black" : "bg-[#020202a8] text-white"}`}
-        >
-          <CardHeader>
-            <CardTitle>Overview</CardTitle>
-            <CardDescription>
-              View your key metrics and recent project activity. Track progress
-              across all your active projects.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="text-muted-foreground text-sm">
-            You have 12 active projects and 3 pending tasks.
-          </CardContent>
-        </Card>
+        <section className="p-5 w-full flex flex-col gap-10 lg:flex-row">
+          <Card
+            title="Essential"
+            description="Simple, focused, effective."
+            badgeText="best"
+            price="950"
+            priceDescription="Perfect for personal sites and small teams."
+            buttonText="Start with Essential"
+            items={[
+              "Custom one-page website design",
+              "Fully responsive layout",
+              "Light animations & interactions",
+              "Webflow CMS setup (optional)",
+              "SEO-ready structure",
+            ]}
+          ></Card>
+          <Card
+            title="Elevate"
+            description="For stories that deserve motion."
+            price="1,950"
+            priceDescription="Best for growing businesses and creative studios."
+            buttonText="Start with Elevate"
+            items={[
+              "Multi-page website (up to 5 pages)",
+              "Custom UI/UX design",
+              "Advanced GSAP animations",
+              "CMS & dynamic collections",
+              "Launch support & training",
+            ]}
+          ></Card>
+        </section>
       </TabsContent>
-      {/* analytics card */}
+      {/* Professional card */}
       <TabsContent
         className="mt-20 w-fit mx-auto flex justify-center flex-wrap gap-10"
-        value="analytics"
+        value="Yearly"
       >
         <Card
-          className={`border border-[#a1a1a14f] bg-[#fff0]  w-[90%] md:w-[40%] backdrop-blur-lg ${theme ? "bg-[#d3d3d33a] text-black" : "bg-[#020202a8] text-white"}`}
-        >
-          <CardHeader>
-            <CardTitle>analytics</CardTitle>
-            <CardDescription>
-              View your key metrics and recent project activity. Track progress
-              across all your active projects.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="text-muted-foreground text-sm">
-            You have 12 active projects and 3 pending tasks.
-          </CardContent>
-        </Card>
+          title="Essential"
+          description="Simple, focused, effective."
+          badgeText="best"
+          price="750"
+          priceDescription="Perfect for personal sites and small teams."
+          buttonText="Start with Essential"
+          items={[
+            "Custom one-page website design",
+            "Fully responsive layout",
+            "Light animations & interactions",
+            "Webflow CMS setup (optional)",
+            "SEO-ready structure",
+          ]}
+        ></Card>
         <Card
-          className={`border border-[#a1a1a14f] bg-[#fff0]  w-[90%] md:w-[40%] backdrop-blur-lg ${theme ? "bg-[#d3d3d33a] text-black" : "bg-[#020202a8] text-white"}`}
-        >
-          <CardHeader>
-            <CardTitle>analytics</CardTitle>
-            <CardDescription>
-              View your key metrics and recent project activity. Track progress
-              across all your active projects.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="text-muted-foreground text-sm">
-            You have 12 active projects and 3 pending tasks.
-          </CardContent>
-        </Card>
+          title="Elevate"
+          description="For stories that deserve motion."
+          price="1,750"
+          priceDescription="Best for growing businesses and creative studios."
+          buttonText="Start with Elevate"
+          items={[
+            "Multi-page website (up to 5 pages)",
+            "Custom UI/UX design",
+            "Advanced GSAP animations",
+            "CMS & dynamic collections",
+            "Launch support & training",
+          ]}
+        ></Card>
       </TabsContent>
     </Tabs>
   );
